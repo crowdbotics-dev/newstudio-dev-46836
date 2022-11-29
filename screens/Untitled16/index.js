e2609c9bc21b541c5a3bae0409d81871c544588b
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-11-29 12:13:50 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled16/index.js
@@ -0,0 +1,26 @@
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled16 = () => {
+  return <View style={_styles.myxtloHe}>
+      <Text style={_styles.vhqhgmLv}>Lorem ipsum…</Text></View>;
+};
+
+export default Untitled16;
+
+const _styles = StyleSheet.create({
+  myxtloHe: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  vhqhgmLv: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
