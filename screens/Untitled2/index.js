8f054e507f25b8dec3ec8c0c583314901c1d895b
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-12-01 23:50:14 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,27 @@
+import { Text } from "react-native";
+import { Pressable } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={_styles.PbYZCVZF}>
+      <Pressable onPress={{}}><View style={_styles.vOfffiSs}></View></Pressable><Pressable onPress={{}}><Text>Lorem ipsum…</Text></Pressable></View>;
+};
+
+export default Untitled2;
+
+const _styles = StyleSheet.create({
+  PbYZCVZF: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  vOfffiSs: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
